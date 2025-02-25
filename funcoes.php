38ecbe68242415afa1b885ca51211baf34611797
ana <johnnysbf122@gmail.com>
2025-02-24 21:15:08 -0300

    Add files via upload

--- funcoes.php
+++ funcoes.php
@@ -6,6 +6,38 @@
     <title>FUNÇÕES PHP</title>
 </head>
 <body>
-    
+    <?php
+    echo "<h3> FUNÇÃO SIMPLES SEM PARAMETRO: </h3>";
+
+    function exibirSaudacao(){
+       echo "<p>Olá! Bem-vindo(a) à disciplina de programção web </p>";
+    }
+
+    exibirSaudacao();
+
+    echo"</hr>";
+
+    echo "<h3> FUNÇÃO COM PARAMENTRO: </h3>";
+    function exibirSaudacaoNome ($nome){
+        echo "<p>Olá, $nome! Bem-vindo(a) à disciplina de programção web</p>";
+   
+    }
+    //chamndo a fução com diferentes valores
+    exibirSaudacaoNome("Salomão");
+    exibirSaudacaoNome("Raissa");
+
+    echo"</hr>";
+
+    echo "<h3> FUNÇÃO COM RETORNO DE VALORES: </h3>";
+    function efetuarSoma ($a, $b){
+        return ($a + $b);
+    }
+
+    //armazena o retorno da função em uma variavel
+
+    $resultado = efetuarSoma(5, 10);
+    echo "<p>A soma dos valores é: $resultado </p>";
+
+    ?>
 </body>
 </html>
